a5ca64200ac1b105b8fdc7cc3ddceca6f783d3d5
GaziMaksudur <gazimaksudur2@gmail.com>
2024-11-07 23:54:32 +0600

    some questions are partially solved

--- src/problem-5.ts
+++ src/problem-5.ts
@@ -1,8 +1,12 @@
 // Write a generic function getProperty that takes an object and a property name as arguments and returns the property value. Add a constraint to ensure the property name exists on the object.
-
-// Sample Input:
-const person = { name: "Alice", age: 30 };
-console.log(getProperty(person, "name"));
+{
+  function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+  }
+  // Sample Input:
+  const person = { name: "Alice", age: 30 };
+  console.log(getProperty(person, "name"));
+}
 
 // Sample Output:
-Alice;
+// Alice;
--- src/problem-7.ts
+++ src/problem-7.ts
@@ -1,8 +1,24 @@
 // Create a TypeScript class Car with properties make, model, and year. Include a method getCarAge that returns the car's age based on the current year.
 
+class Car {
+    make: string;
+    model: string;
+    year: number;
+
+    constructor(make: string, model: string, year: number) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    getCarAge(): number {
+        const curYear = new Date().getFullYear();
+        return curYear - this.year;
+    }
+}
 // Sample Input 1:
 const car = new Car("Honda", "Civic", 2018);
-car.getCarAge();
+console.log(car.getCarAge());
 
 // Sample Output 1:
-6 (assuming current year is 2024)
+// 6 (assuming current year is 2024)
